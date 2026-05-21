4e5813d6bcd1d514000707aa20d5445642297e20
Pawel Krol <paullkroll@icloud.com>
2026-05-21 18:48:48 +0200

    add providers into app spec file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,13 @@
-import {Component, effect, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderFeature} from '../../project-features/shared/shared-features/header-feature/header-feature';
-import {LangChangeService} from '../../project-features/shared/services/lang-change.service';
 import {FooterFeature} from '../../project-features/shared/shared-features/footer-feature/footer-feature';
-import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderFeature, FooterFeature],
-  providers: [TranslateService],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  readonly langChangeService = inject(LangChangeService);
 }
