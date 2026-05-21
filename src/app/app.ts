833d3636e5e742f22eb07fbd9aabd41c3504c11b
Pawel Krol <paullkroll@icloud.com>
2026-05-21 18:47:03 +0200

    add provider

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import {RouterOutlet} from '@angular/router';
 import {HeaderFeature} from '../../project-features/shared/shared-features/header-feature/header-feature';
 import {LangChangeService} from '../../project-features/shared/services/lang-change.service';
 import {FooterFeature} from '../../project-features/shared/shared-features/footer-feature/footer-feature';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderFeature, FooterFeature],
+  providers: [TranslateService],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
