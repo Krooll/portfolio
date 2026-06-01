4faa8655c100bc13ca4a632eb0474eb9b53ef817
Pawel Krol <paullkroll@icloud.com>
2026-06-01 13:09:09 +0200

    end work for tech

--- project-features/features/technology-page/technology-page.ts
+++ project-features/features/technology-page/technology-page.ts
@@ -1,9 +1,81 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateFallbackPipe} from "../../shared/pipes/translate-pipe";
+
+export interface TechnologyItem {
+  icon: string;
+}
+
+export interface AiItem {
+  icon: string;
+}
 
 @Component({
   selector: 'app-technology-shell-page',
-  imports: [],
+  imports: [
+    TranslateFallbackPipe
+  ],
   templateUrl: './technology-page.html',
   styleUrl: './technology-page.scss',
 })
-export class TechnologyPage {}
+export class TechnologyPage {
+
+  technologyItems: TechnologyItem[] = [
+    {
+      icon: '/angular.png',
+    },
+    {
+      icon: '/ts.png',
+    },
+    // {
+    //   icon: '/rxjs.png',
+    // },
+    {
+      icon: '/js.png',
+    },
+    {
+      icon: '/nx.svg',
+    },
+    {
+      icon: '/boostrap.png',
+    },
+    {
+      icon: '/html.png',
+    },
+    {
+      icon: '/css.png',
+    },
+    {
+      icon: '/jest.svg',
+    },
+    {
+      icon: '/cypress.svg',
+    },
+    {
+      icon: '/npm.png',
+    },
+    {
+      icon: '/keycloak.png',
+    },
+    {
+      icon: '/docker.png',
+    },
+    {
+      icon: '/figma.png',
+    },
+    {
+      icon: '/jira.png',
+    },
+  ];
+
+  aiItems: AiItem[] = [
+    {
+      icon: '/codex.png',
+    },
+    {
+      icon: '/perplexity.png',
+    },
+    {
+      icon: '/gemini.png',
+    }
+  ];
+}
--- project-features/shells/technology-shell/technology-shell.ts
+++ project-features/shells/technology-shell/technology-shell.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-technology-shell',
-  imports: [],
+    imports: [
+        RouterOutlet
+    ],
   templateUrl: './technology-shell.html',
   styleUrl: './technology-shell.scss',
 })
